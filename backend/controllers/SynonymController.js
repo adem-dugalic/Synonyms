08e43bc231217bccc62cd6cf8fed4bbe4dac8374
skeri123 <31672243+skeri123@users.noreply.github.com>
2022-12-03 17:38:25 +0100

    C-9 standardise input to prevent duplicates

--- backend/controllers/SynonymController.js
+++ backend/controllers/SynonymController.js
@@ -6,8 +6,10 @@ let map = new Map();
 // @access public
 
 const getSpecificSynonyms = async (req, res) => {
-  if (map.has(req.params.key)) {
-    return res.status(200).json(allSynonymGroups[map.get(req.params.key)]);
+  if (map.has(req.params.key.toUpperCase())) {
+    return res
+      .status(200)
+      .json(allSynonymGroups[map.get(req.params.key.toUpperCase())]);
   } else {
     return res.status(404).json({ msg: "Not fund sry!" });
   }
@@ -20,48 +22,49 @@ const getSpecificSynonyms = async (req, res) => {
 const setSynonyms = async (req, res) => {
   //Map has to contain index for specific word which
   //corresponds to the specific array in the list
-
+  const word = req.body.word.toUpperCase();
+  const synonym = req.body.synonym.toUpperCase();
   //error handling in case req.body is empty
-  if (!req.body.word || !req.body.synonym) {
+  if (!word || !synonym) {
     return res.status(400).json({ message: "Please add synonym and word" });
   }
   //first element
   if (allSynonymGroups.length === 0) {
-    map.set(req.body.word, 0); // replace temp with index
-    map.set(req.body.synonym, 0);
-    allSynonymGroups.push([req.body.word, req.body.synonym]);
+    map.set(word, 0); // replace temp with index
+    map.set(synonym, 0);
+    allSynonymGroups.push([word, synonym]);
     return res.status(201).json({ message: "Addded synonym" });
   }
 
   //now for all other inserts we need to do checks
   //--------------------------------------------------------
   //check if we have one of these or both
-  if (map.has(req.body.word)) {
-    if (map.has(req.body.synonym)) {
+  if (map.has(word)) {
+    if (map.has(synonym)) {
       res.status(400).json({ message: "Synonym already exist" });
       return;
     }
-    allSynonymGroups[map.get(req.body.word)].push(req.body.synonym);
-    map.set(req.body.synonym, map.get(req.body.word));
+    allSynonymGroups[map.get(word)].push(synonym);
+    map.set(synonym, map.get(word));
     res.status(201).json({ message: "Addded synonym" });
     return;
   }
   //secod case
-  if (map.has(req.body.synonym)) {
-    if (map.has(req.body.word)) {
+  if (map.has(synonym)) {
+    if (map.has(word)) {
       res.status(400).json({ message: "Synonym already exist" });
       return;
     }
-    allSynonymGroups[map.get(req.body.synonym)].push(req.body.word);
-    map.set(req.body.word, map.get(req.body.synonym));
+    allSynonymGroups[map.get(synonym)].push(word);
+    map.set(word, map.get(synonym));
     res.status(201).json({ message: "Addded synonym" });
     return;
   }
   //--------------------------------------------------------
   //Now we can insert a new set of synonyms
-  allSynonymGroups.push([req.body.word, req.body.synonym]);
-  map.set(req.body.word, allSynonymGroups.length - 1);
-  map.set(req.body.synonym, allSynonymGroups.length - 1);
+  allSynonymGroups.push([word, synonym]);
+  map.set(word, allSynonymGroups.length - 1);
+  map.set(synonym, allSynonymGroups.length - 1);
 
   res.status(201).json({ message: "Addded synonym" });
   return;
